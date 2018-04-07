f7d1e6986b3e0996262fdefb41ea8a7355b5394f
Dan Searle <dan.searle95@live.co.uk>
2018-04-07 20:56:41 +0100

    MVC setup.

---
+++ www/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace www
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection Bundles)
+        {
+            bundles.Add(new StyleBundle("~/Styles").Include(
+                      "~/Content/custom.css"));
+        }
+    }
+}
